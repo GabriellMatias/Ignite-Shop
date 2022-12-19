e48b6ecfe1520257cd662fc1555472ec6626f548
GabriellMatias <gabriel.goiasat@gmail.com>
2022-12-19 13:53:05 -0300

    :round_pushpin: First commit

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,8 @@
+/* useRouter tem acesso aos parametros que vem de [id] que e o que vem depois de 
+product por exemplo na URL*/
+
+export default function Product(){
+  return(
+    <h1>Product</h1>
+  )
+}
