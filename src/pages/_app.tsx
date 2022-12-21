31e268e10a4a53851ea881c7196d4734cbb96c16
GabriellMatias <gabriel.goiasat@gmail.com>
2022-12-20 21:32:27 -0300

    :hammer: Feature: Finished basics settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+import logoImg from '../assets/Logo.svg'
+import { Container, Header } from '../styles/pages/app'
+import Image from 'next/image'
+/* estilizacao global no next com stitches*/
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  
+
+  return (
+    <Container>
+      <Header>
+        {/* otimiza a imgem para ficar muito mais leve */}
+          <Image src={logoImg.src} alt="" />
+      </Header>
+  <Component {...pageProps} />
+  </Container>)
 }
