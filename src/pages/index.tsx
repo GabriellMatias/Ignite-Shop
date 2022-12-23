de98e049de26d97592f23deb51c4fcb7b10d4899
GabriellMatias <gabriel.goiasat@gmail.com>
2022-12-23 18:31:24 -0300

    :hammer: Feature: Finished produtc page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,14 @@ import 'keen-slider/keen-slider.min.css'
 import { GetStaticProps } from 'next'
 import { stripe } from '../lib/stripe'
 import Stripe from 'stripe'
+import Link from 'next/link'
 
 interface ProductDataProps {
   productData: {
     id: string
     name: string
     imgUrl: string
-    price: number
+    price: string
   }[]
   /* colchete no final significa que esse dado e um array de produtos, ou seja
   tem varios */
@@ -31,13 +32,24 @@ export default function Home({ productData }: ProductDataProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {productData.map((product) => {
         return (
-          <Product className="keen-slider__slide" key={product.id}>
-            <Image src={product.imgUrl} width={520} height={480} alt="" />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          /* link do next impede que coisas desnecessaria sejam carregadas
+          quando o usuario clicar no link */
+          <Link
+            href={`/product/${product.id}`}
+            key={product.id}
+            /* um complemento para funcionar o getStaticPath, para que ele nao 
+            pre Carregue a pagina. Prefetch se deixar isso deixa pesado pois ele pre-carrega a pagina
+            automaticamente */
+            prefetch={false}
+          >
+            <Product className="keen-slider__slide">
+              <Image src={product.imgUrl} width={520} height={480} alt="" />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,89 @@
 /* useRouter tem acesso aos parametros que vem de [id] que e o que vem depois de 
 product por exemplo na URL */
 
-export default function Product() {
-  return <h1>Product</h1>
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Image from 'next/image'
+import Stripe from 'stripe'
+import { stripe } from '../../lib/stripe'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
+
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imgUrl: string
+    price: string
+    description: string
+  }
+}
+
+export default function Product({ product }: ProductProps) {
+  // const { isFallback } = useRouter() retorna se a pagina ta sendo carregada caso o fallback seja true
+
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={product.imgUrl} alt="" width={520} height={480} />
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
+
+        <button>Buy now</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
+}
+
+/* paginas dinamicas sao obrigatorias o getStaticPath */
+/* dizer quais parametros vao vim pra gerar as paginas staticas quando a aplicacao
+comeca */
+export const getStaticPath: GetStaticPaths = async () => {
+  /* busca os produtos mais acessados e passa em paths os produtos mais acessados
+  pois essa paginas serao geradas no momento da build */
+
+  /* passa o fallback true para quando o usuario acessar uma pagina que nao passei em paths
+  nao da erro 404 */
+  /* fallback: true > tenta executar o getStaticProps antes de renderizar as paginas
+  que nao foram passadas no path, no entanto nesse metodo precisa de uma tela de carreegamento */
+
+  return {
+    paths: [{ params: { id: 'prod_N1vkwmgVKKQQjJ' } }],
+    fallback: true,
+  }
+}
+
+/* pode pegar as propriedades que sao passadas para o componente, desestruturando os 
+params por aqui, assim fica mais facil de fazer a requisicao de acordo com o produto */
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params?.id
+  const product = await stripe.products.retrieve(productId!, {
+    expand: ['default_price'],
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      id: product.id,
+      name: product.name,
+      /* pode ter varias imagens, entao pega a primeira cm o vetor */
+      imgUrl: product.images[0],
+      /* vem em centavos */
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount! / 100),
+      description: product.description,
+    },
+    revalidate: 60 * 60 * 4, // 4horas
+  }
 }
